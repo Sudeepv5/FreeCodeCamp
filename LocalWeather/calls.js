850000fa989781bf4e8763914d0919202b77efa8
Sudeepv5 <sudeepv5@ccs.neu.edu>
2015-08-25 12:24:46 -0400

    Second Zipline
    Completed Local Weather

--- LocalWeather/calls.js
+++ LocalWeather/calls.js
@@ -1,13 +1,34 @@
 $(document).ready(function(){
     
+    $("#panel").hide();
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(callZeus);
     } else { 
         $("#comment").html("Belurbbbeh! Weather so unpredictable that it broke our computers!");
     }
+    
+    
+    $("#f-toggle").click(function(){
+        $("#c-toggle").removeClass("active");
+        $("#f-toggle").addClass("active");
+        $("#temp").html(tempF);                  
+    });
+    $("#c-toggle").click(function(){
+        $("#f-toggle").removeClass("active");
+        $("#c-toggle").addClass("active");
+       $("#temp").html(tempC);
+    });
+    $("#d-toggle").click(function(){
+        if($("#details").is(":visible"))
+            $("#details").hide();
+        else
+            $("#details").show();
+    });
+    
 });
 
-
+var tempC=0;
+var tempF=0;
 function callZeus(position){
     var qUrl='https://simple-weather.p.mashape.com/weatherdata';
     $.ajax({
@@ -17,7 +38,8 @@ function callZeus(position){
         data:{"lat":position.coords.latitude,
               "lng":position.coords.longitude},
         success:function(response){
-            sendThunders(response)
+            console.log(response);
+            sendThunders(response);
         },
         error:function(){
             $("#comment").html("Belurbbbeh! Weather so unpredictable that it broke our computers!");
@@ -31,9 +53,14 @@ function callZeus(position){
 function sendThunders(report)
 {
     var prb=report.query.results.channel;
-    $("#location").html(prb.location.city+", "+prb.location.country);
-    $("#w-icon").html(prb.item.description);
+    $("#location").html(prb.location.city+", "+((prb.location.region==="") ? prb.location.country : prb.location.region));
     $("#temp").html(prb.item.condition.temp);
     $("#comment").html(prb.item.condition.text);
+    $("#details").html(prb.item.description);
+    $("#details").hide();
+    $("#panel").show();
+    tempC=prb.item.condition.temp;
+    tempF=Math.round((tempC*9/5) + 32);
+    $("#spinner").hide();
 }
 
